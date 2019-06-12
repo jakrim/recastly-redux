cf5ce1b4f05e7c89c2d0810b90d63fb77df31ceb
jakrim <jesse.krim@gmail.com>
2019-06-12 19:07:55 -0400

    Complete store tests

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,8 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./../reducers/main.js";
-import exampleVideoData from "../data/exampleVideoData.js";
 
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
-let store = createStore(rootReducer);
+let store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
