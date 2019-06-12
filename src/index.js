6ccf54e8eb5a63988dab6b915b4a909674e285bf
jakrim <jesse.krim@gmail.com>
2019-06-12 15:31:05 -0400

    Pass tests for action dispatchers

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App.js';
-import searchYouTube from './lib/searchYouTube.js';
-import YOUTUBE_API_KEY from './config/youtube.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App.js";
+import searchYouTube from "./lib/searchYouTube.js";
+import YOUTUBE_API_KEY from "./config/youtube.js";
 
 //TODO: Import the Provider component from 'react-redux' here!
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 //TODO: Use the Provider component to make your store available to
 //  the rest of your app.
 
 ReactDOM.render(
-  <App API_KEY={YOUTUBE_API_KEY} searchYouTube={searchYouTube} />,
-  document.getElementById('app')
+  <Provider store={store}>
+    <App API_KEY={YOUTUBE_API_KEY} searchYouTube={searchYouTube} />
+  </Provider>,
+  document.getElementById("app")
 );
--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -1,7 +1,16 @@
-import Redux from 'redux';
+// import Redux from "redux";
+// import changeVideo from "../actions/currentVideo";
 
-var currentVideoReducer = (state, action) => {
+var init = { video: [] };
+
+var currentVideoReducer = (state = init, action) => {
   //TODO: define a reducer for the currentVideo field of our state.
+  switch (action.type) {
+    case "CHANGE_VIDEO":
+      return Object.assign({}, state, { video: action.video });
+    default:
+      return state;
+  }
 };
 
 export default currentVideoReducer;
