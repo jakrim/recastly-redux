412b2c3e72c5147ff3290813ff0e72375cc2d8f6
“Jesse <jesse.krim@gmail.com>
2019-08-13 14:42:27 -0400

    Fix Searchyoutube function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App.js";
-import searchYouTube from "./lib/searchYouTube.js";
-import YOUTUBE_API_KEY from "./config/youtube.js";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.js';
+import searchYouTube from './lib/searchYouTube.js';
+import YOUTUBE_API_KEY from './config/youtube.js';
 
 //TODO: Import the Provider component from 'react-redux' here!
-import { Provider } from "react-redux";
-import store from "./store/store";
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 //TODO: Use the Provider component to make your store available to
 //  the rest of your app.
@@ -15,5 +15,5 @@ ReactDOM.render(
   <Provider store={store}>
     <App API_KEY={YOUTUBE_API_KEY} searchYouTube={searchYouTube} />
   </Provider>,
-  document.getElementById("app")
+  document.getElementById('app')
 );
