d8fcc1a47c6d68b6582836efbeb99c57d9dc9206
Jesse Krim <jesse.krim@gmail.com>
2019-10-08 15:05:15 -0400

    Restore package.json

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,20 @@
-import React from "react";
-import VideoList from "../containers/VideoListContainer";
-import VideoPlayer from "../containers/VideoPlayerContainer";
-import Search from "../containers/SearchContainer";
+import React from 'react';
+import VideoList from '../containers/VideoListContainer';
+import VideoPlayer from '../containers/VideoPlayerContainer';
+import Search from '../containers/SearchContainer';
 
 var App = props => (
   <div>
-    <nav className="navbar">
-      <div className="col-md-6 col-md-offset-3">
+    <nav className='navbar'>
+      <div className='col-md-6 col-md-offset-3'>
         <Search />
       </div>
     </nav>
-    <div className="row">
-      <div className="col-md-7">
+    <div className='row'>
+      <div className='col-md-7'>
         <VideoPlayer />
       </div>
-      <div className="col-md-5">
+      <div className='col-md-5'>
         <VideoList />
       </div>
     </div>
